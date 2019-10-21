ac1522dd48bd9c7d8835392026be2cb968114ccb
Noble Yang <775488842@qq.com>
2019-10-21 11:36:11 +0800

    jsp动作

---
+++ JavaWebTP09/src/main/java/cn/com/demo/tp09/servlet/DemoServlet.java
@@ -0,0 +1,25 @@
+package cn.com.demo.tp09.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet("/demoServlet")
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().write("<br/>hello servlet<br/>");
+	}
+
+}
---
+++ JavaWebTP09/src/main/webapp/footer.jsp
@@ -0,0 +1,3 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<br/>footer************************<%int i1=3; %><%=i1%>
